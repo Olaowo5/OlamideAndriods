c95a19aee94a03747a8e86ee3b5fd78b6bc6abc6
Olaowo5 <olaowolabi@hotmail.co.uk>
2023-02-06 23:08:47 +0100

    week 4 da

--- app/src/main/java/algonquin/cst2335/owol/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/owol/MainActivity.java
@@ -11,31 +11,28 @@ import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imgView;
-    Switch sw;
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onStop() {
+        super.onStop();
+    }
 
-        imgView = findViewById(R.id.flagView);
-        sw = findViewById(R.id.spin_switch);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
-        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
 
-            if (isChecked)
-            {
-                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-                rotate.setDuration(5000);
-                rotate.setRepeatCount(Animation.INFINITE);
-                rotate.setInterpolator(new LinearInterpolator());
-
-                imgView.startAnimation(rotate);
-            }
-            else {
-                imgView.clearAnimation();
-            }
-        });
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
