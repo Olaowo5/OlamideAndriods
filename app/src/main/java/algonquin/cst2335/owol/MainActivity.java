6fc2688d312d8436ec734b24aa8e281910425131
Olaowo5 <olaowolabi@hotmail.co.uk>
2023-02-13 23:43:24 +0100

    Early commit

--- app/src/main/java/algonquin/cst2335/owol/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/owol/MainActivity.java
@@ -3,6 +3,9 @@ package algonquin.cst2335.owol;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.textView);
+        EditText PT = findViewById(R.id.editPass);
+        Button Btn = findViewById(R.id.lognButton);
+
+        Btn.setOnClickListener(clk -> {
+            String ThePass = PT.getText().toString();
+            checkPasswordComplexitiy(ThePass);
+        });
+    }
+
+    /**
+     * Used in checking password complexity
+     * @param pw The String Object we are checking
+     */
+    void checkPasswordComplexitiy(String pw)
+    {
+
     }
 }
