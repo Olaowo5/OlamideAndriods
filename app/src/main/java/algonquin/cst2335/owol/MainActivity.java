0a241c29e7e8bf858c67c3c8d8bf08c7c91a9cb3
Olaowo5 <olaowolabi@hotmail.co.uk>
2023-03-23 19:46:09 +0100

    Finished Week10 in JSON

--- app/src/main/java/algonquin/cst2335/owol/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/owol/MainActivity.java
@@ -2,11 +2,13 @@ package algonquin.cst2335.owol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -42,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
     protected String cityName;
    protected RequestQueue queue = null;
 
+   protected  Bitmap ImagOla = null;
+   protected String imageUrl = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +55,30 @@ public class MainActivity extends AppCompatActivity {
 
         ActivityMainBinding binding = ActivityMainBinding.inflate( getLayoutInflater() );
         setContentView(binding.getRoot());
+
+
+
+        runOnUiThread( (  )  -> {
+            binding.temp.setVisibility(View.INVISIBLE);
+
+
+            binding.Mintemp.setVisibility(View.INVISIBLE);
+
+
+            binding.Maxtemp.setVisibility(View.INVISIBLE);
+
+
+            binding.humidity.setVisibility(View.INVISIBLE);
+
+
+            binding.description.setVisibility(View.INVISIBLE);
+
+
+            //binding.icon.setImageBitmap(ImagOla);
+            binding.icon.setVisibility(View.INVISIBLE);
+            // do this for all the textViews...
+
+        });
 
         binding.forecastButton.setOnClickListener(click -> {
             cityName = binding.cityfield.getText().toString();
@@ -62,6 +90,11 @@ public class MainActivity extends AppCompatActivity {
             } catch (UnsupportedEncodingException e) {
                 throw new RuntimeException(e);
             }
+
+
+
+
+
             String URL = null;
             //https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${KeyName}
             URL = "https://api.openweathermap.org/data/2.5/weather?q=" + stringURL + "&units=metric&appid=972f4a9632b1de8f2d0f4037996c1e53";
@@ -99,42 +132,80 @@ public class MainActivity extends AppCompatActivity {
                     int vis = response.getInt("visibility");
                     String name = response.getString("name");
 
+                    //Binding Section
+
+                   
+
+                   
                     //IMAGE
 
                     String pathName = getFilesDir() + "/" + iconName + ".png";
                     File file = new File(pathName);
-                    Bitmap image = null;
+
                     if (file.exists()) {
-                        image = BitmapFactory.decodeFile(pathName);
+                        ImagOla = BitmapFactory.decodeFile(pathName);
                     }
                     else {
-                        String imageUrl = "https://openweathermap.org/img/w/" + iconName + ".png";
+                        imageUrl = "https://openweathermap.org/img/w/" + iconName + ".png";
 
+
+                       // Bitmap finalImage = imagOla;
                         ImageRequest imgReq = new ImageRequest(imageUrl, new Response.Listener<Bitmap>() {
                             @Override
                             public void onResponse(Bitmap bitmap) {
                                 // Do something with loaded bitmap...
-
-
-                                {
-                                    FileOutputStream fOut = null;
+                                    String lp = "Kp";
+                                  //  FileOutputStream fOut = null;
                                     try {
-                                        fOut = openFileOutput(iconName + ".png", Context.MODE_PRIVATE);
+                                      //  fOut = openFileOutput(iconName + ".png", Context.MODE_PRIVATE);
+                                        ImagOla = bitmap;
 
-                                        image.compress(Bitmap.CompressFormat.PNG, 100, fOut);
-                                        fOut.flush();
-                                        fOut.close();
+                                        Bitmap temp = bitmap;
+                                        ImagOla.compress(Bitmap.CompressFormat.PNG, 100,
+                                                MainActivity.this.openFileOutput(iconName+".png", Activity.MODE_PRIVATE));
+                                       // fOut.flush();
+                                      //  fOut.close();
+                                        
+                                       
                                     } catch (IOException e) {
                                         e.printStackTrace();
 
                                     }
-                                }
+
                             }
                         }, 1024, 1024, ImageView.ScaleType.CENTER, null, (error) -> {
 
                         });
 
+                        queue.add(imgReq);
+
                     }
+
+
+
+                    runOnUiThread( (  )  -> {
+
+                        binding.temp.setText("The Current Temperature is "+ current);
+                        binding.temp.setVisibility(View.VISIBLE);
+
+                        binding.Mintemp.setText("The Minimum Temperature is " + min);
+                        binding.Mintemp.setVisibility(View.VISIBLE);
+
+                        binding.Maxtemp.setText("The Maximum Temeperature is " + max);
+                        binding.Maxtemp.setVisibility(View.VISIBLE);
+
+                        binding.humidity.setText("The Humidity is " + humidity);
+                        binding.humidity.setVisibility(View.VISIBLE);
+
+                        binding.description.setText("Description : " + descrip);
+                        binding.description.setVisibility(View.VISIBLE);
+
+
+                        binding.icon.setImageBitmap(ImagOla);
+                        binding.icon.setVisibility(View.VISIBLE);
+                        // do this for all the textViews...
+
+                    });
                 }
                 catch ( JSONException e) {
                     throw new RuntimeException(e);
@@ -144,6 +215,9 @@ public class MainActivity extends AppCompatActivity {
                     (error)->{ } );
 
             queue.add(request);
+            
+            
+            
 
         });
     }
